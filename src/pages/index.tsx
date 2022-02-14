2abc6c92cd9e93089f0b25a5bb387780f0cea4ac
Igor Santos <igorsantoscosta@gmail.com>
2022-02-14 14:20:50 -0300

    Create project

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import styles from '../styles/Home.module.scss'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Hello World!!</h1>
+      <h2>Hello World!!</h2>
+      <h3>Hello World!!</h3>
+      <h4>Hello World!!</h4>
+      <h5>Hello World!!</h5>
+      <h6>Hello World!!</h6>
+      <p>Hello World!!</p>
+    </div>
+  )
+}
+
+export default Home
